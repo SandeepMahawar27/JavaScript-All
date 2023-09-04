9586a7c606085d60f5b008c7afaf3e4bed4fde5d
Sandeep Mahawar <133506821+SandeepMahawar27@users.noreply.github.com>
2023-09-04 10:43:46 +0000

    conversion of datatypes

---
+++ JS_Basics/conversionOperations.js
@@ -0,0 +1,32 @@
+let score = "33acb";
+
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score)
+
+// console.log(valueInNumber);   //NAN
+// console.log(typeof(valueInNumber));
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 1;
+let isLoggedIn = ""
+
+let boolenIsLoogedIn = Boolean(isLoggedIn);
+// console.log(boolenIsLoogedIn);
+
+// 1 => true
+// "" => false
+//  "Sandeep" => true
+
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+
+// console.log(stringNumber);          // 33
+// console.log(typeof stringNumber);   // String
