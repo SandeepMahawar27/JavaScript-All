9807c13c56bb5859febc0dacec47c8daf3e93927
Sandeep Mahawar <133506821+SandeepMahawar27@users.noreply.github.com>
2023-09-04 11:11:30 +0000

    Conversion and Opersions

--- JS_Basics/conversionOperations.js
+++ JS_Basics/conversionOperations.js
@@ -30,3 +30,50 @@ let stringNumber = String(someNumber);
 
 // console.log(stringNumber);          // 33
 // console.log(typeof stringNumber);   // String
+
+
+//  ******************************* Operations **************************** 
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue)   // -3
+
+// console.log(2+2); // 4
+// console.log(2*2); //4
+// console.log(2-2); //0
+// console.log(2/2); //1
+// console.log(2**2); //4
+
+let str1 = "Hello";
+let str2 = "Sandeep";
+
+// console.log(str1 + " " + str2);
+
+// console.log("1" + 2);           //12
+// console.log(1 + "2");           //12
+// console.log("1" + 2 + 2);       //122
+// console.log(1 + 2 +"2");        //32
+
+// console.log(+true);   // 1
+// console.log(+"");     //0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+// ++gameCounter;
+gameCounter++;
+// console.log(gameCounter);
+
+
+          // that's are a example of prefix and postfix 
+
+let x = 3;
+const y = x++;
+console.log(x,y);
+
+let a = 3;
+const b = ++a; 
+console.log(a,b);
