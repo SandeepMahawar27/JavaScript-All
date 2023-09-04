368d78187cc2d71a3386f0a3c3f24d8494ac081f
Sandeep Mahawar <133506821+SandeepMahawar27@users.noreply.github.com>
2023-09-04 12:52:27 +0000

    Adding some comparisons rules

---
+++ JS_Basics/comparisons.js
@@ -0,0 +1,29 @@
+// // basic comparisons
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// Strings conversions
+
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// some others conversions
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+// console.log(null < 0)
+
+
+// console.log(undefined == 0)
+// console.log(undefined > 0)
+// console.log(undefined < 0)
+
+
+// === (strictly check )
+console.log("2" === 2);
+
+
