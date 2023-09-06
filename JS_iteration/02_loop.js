f1ab54852ddffbb54c1a6d106b15150aa34f73fd
Sandeep Mahawar <133506821+SandeepMahawar27@users.noreply.github.com>
2023-09-06 17:04:52 +0000

    while and do while loop

---
+++ JS_iteration/02_loop.js
@@ -0,0 +1,25 @@
+// while loop
+
+let index = 0;
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 2;
+}
+
+let myArr = ["flash", "batman", "spiderman"]
+
+let arr = 0
+while(arr < myArr.length){
+    // console.log(`valuse is ${myArr[arr]}`);
+    arr = arr + 1;
+}
+
+
+// do while loop     yhe loop ak baar jarur chlta hai .......
+
+let score = 11;
+
+do {
+    console.log(`Score is ${score}`);
+    score++;
+} while (score <= 10);
