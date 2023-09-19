f65b5a7e587add7f7fa2335ebb8329b10c536f83
Sandeep Mahawar <133506821+SandeepMahawar27@users.noreply.github.com>
2023-09-19 11:31:28 +0000

    AsyncAndAwait and ThenCatch With fetch function

---
+++ 10_Promises/AsyncAwaitAndPromises.js
@@ -0,0 +1,29 @@
+
+// use of async and await and use the fetch function
+
+// getAllUser = async () => {
+//     try {
+//         const res = await fetch("http://jsonplaceholder.typicode.com/users");
+//         const data = await res.json();
+
+//         console.log(data);
+//     } catch (err) {
+//         console.log(err);
+//     }
+// }
+// getAllUser();
+
+
+
+// use of then and catch with fetch function
+
+// fetch('http://jsonplaceholder.typicode.com/users')
+// .then( (res) => {
+//   return res.json();
+// }).then( (data) => {
+//     console.log(data);
+// })
+// .catch((err) => {
+//     console.log(err);
+// })
+
